5af099885b2fae9b41b4a256aae1392aa33a6a3d
ahmadsyahabdullah <154076787+ahmadsyahabdullah@users.noreply.github.com>
2026-02-19 06:52:24 +0800

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "SekaiDrama - Streaming Drama Pendek",
-  description: "Nonton drama pendek gratis dan tanpa iklan di SekaiDrama.",
+  title: "ASANONTON DRAMA CHINA - AHMAD SYAH ABDULLAH - Streaming Drama Pendek",
+  description: "Nonton drama pendek gratis dan tanpa iklan di ASANONTON DRAMA CHINA - AHMAD SYAH ABDULLAH.",
 };
 
 export default function RootLayout({
